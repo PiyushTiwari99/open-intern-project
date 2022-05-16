3f0d74dcd74a3568eed43d263b8df0ffa6d6169e
PiyushTiwari99 <piyushtiwarii142@gmail.com>
2022-05-17 00:31:26 +0530

    completed

---
+++ src/route/route.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+
+
+const CollegeController = require("../controller/collegeController")
+const InternController = require("../controller/internController")
+
+
+router.post("/functionup/colleges", CollegeController.createCollege)
+router.post("/functionup/interns", InternController.createIntern)
+router.get("/functionup/collegeDetails", CollegeController.getCollege)
+
+
+module.exports = router;
+
+
